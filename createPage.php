67ca1af7673529b74ee92e8e4d5c9a11eb5c78dc
sergej <tulitas1@gmail.com>
2020-10-10 22:38:10 +0300

    Create create page

--- createPage.php
+++ createPage.php
@@ -3,52 +3,53 @@
 require_once "config.php";
 
 // Define variables and initialize with empty values
-$name = $address = $salary = "";
-$name_err = $address_err = $salary_err = "";
+$county = $country = $town = $description = $displayableaddress = $thumbnail = "";
+$county_err = $country_err = $town_err = $description_err = "";
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate name
-    $input_name = trim($_POST["name"]);
-    if(empty($input_name)){
-        $name_err = "Please enter a name.";
-    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $name_err = "Please enter a valid name.";
+    $input_county = trim($_POST["county"]);
+    if(empty($input_county)){
+        $county_err = "Please enter a county.";
+
     } else{
-        $name = $input_name;
+        $county = $input_county;
     }
 
     // Validate address
-    $input_address = trim($_POST["address"]);
-    if(empty($input_address)){
-        $address_err = "Please enter an address.";
+    $input_country = trim($_POST["country"]);
+    if(empty($input_country)){
+        $country_err = "Please enter a country.";
     } else{
-        $address = $input_address;
+        $country = $input_country;
     }
 
     // Validate salary
-    $input_salary = trim($_POST["salary"]);
-    if(empty($input_salary)){
-        $salary_err = "Please enter the salary amount.";
-    } elseif(!ctype_digit($input_salary)){
-        $salary_err = "Please enter a positive integer value.";
-    } else{
-        $salary = $input_salary;
+    $input_town = trim($_POST["town"]);
+    if(empty($input_town)){
+        $town_err = "Please enter the town.";
+    }  else{
+        $town = $input_town;
     }
 
     // Check input errors before inserting in database
-    if(empty($name_err) && empty($address_err) && empty($salary_err)){
+    if(empty($county_err) && empty($country_err) && empty($town_err)){
         // Prepare an insert statement
-        $sql = "INSERT INTO employees (name, address, salary) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO rent (count, country, town, description, displayableaddress, image, thumbnail
+) VALUES (?, ?, ?)";
 
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_address, $param_salary);
+            mysqli_stmt_bind_param($stmt, "sss",
+                $param_county,
+                $param_country,
+                $param_town);
 
             // Set parameters
-            $param_name = $name;
-            $param_address = $address;
-            $param_salary = $salary;
+            $param_county = $county;
+            $param_country = $country;
+            $param_town = $town;
 
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
@@ -92,20 +93,36 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 </div>
                 <p>Please fill this form and submit to add employee record to the database.</p>
                 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                    <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
-                        <label>Name</label>
-                        <input type="text" name="name" class="form-control" value="<?php echo $name; ?>">
-                        <span class="help-block"><?php echo $name_err;?></span>
+                    <div class="form-group <?php echo (!empty($county_err)) ? 'has-error' : ''; ?>">
+                        <label>County</label>
+                        <input type="text" name="county" class="form-control" value="<?php echo $county; ?>">
+                        <span class="help-block"><?php echo $county_err;?></span>
                     </div>
-                    <div class="form-group <?php echo (!empty($address_err)) ? 'has-error' : ''; ?>">
-                        <label>Address</label>
-                        <textarea name="address" class="form-control"><?php echo $address; ?></textarea>
-                        <span class="help-block"><?php echo $address_err;?></span>
+                    <div class="form-group <?php echo (!empty($country_err)) ? 'has-error' : ''; ?>">
+                        <label>Country</label>
+                        <input type="text" name="country" class="form-control"><?php echo $country; ?></input>
+                        <span class="help-block"><?php echo $country_err;?></span>
                     </div>
-                    <div class="form-group <?php echo (!empty($salary_err)) ? 'has-error' : ''; ?>">
-                        <label>Salary</label>
-                        <input type="text" name="salary" class="form-control" value="<?php echo $salary; ?>">
-                        <span class="help-block"><?php echo $salary_err;?></span>
+                    <div class="form-group <?php echo (!empty($town_err)) ? 'has-error' : ''; ?>">
+                        <label>Town</label>
+                        <input type="text" name="town" class="form-control" value="<?php echo $town; ?>">
+                        <span class="help-block"><?php echo $town_err;?></span>
+                    </div>
+                    <div class="form-group <?php echo (!empty($description_err)) ? 'has-error' : ''; ?>">
+                        <label>Description</label>
+                        <input type="text" name="description" class="form-control" value="<?php echo $description; ?>">
+
+                    </div>
+                    <div class="form-group <?php echo (!empty($displayableaddress_err)) ? 'has-error' : ''; ?>">
+                        <label>Displayable Address</label>
+                        <input type="text" name="displayableaddress" class="form-control" value="<?php echo $displayableaddress; ?>">
+
+                    </div>
+                    <input type="file" name="image" />
+                    <div class="form-group ">
+                        <label>Thumbnail</label>
+                        <input type="text" name="thumbnail" class="form-control" value="<?php echo $thumbnail; ?>">
+
                     </div>
                     <input type="submit" class="btn btn-primary" value="Submit">
                     <a href="index.php" class="btn btn-default">Cancel</a>
